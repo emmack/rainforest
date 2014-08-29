e8d12b61ade581e0e14dd223cf8e58712d9fb922
emmack <eckotzer@gmail.com>
2014-08-29 14:50:03 -0400

    format price

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,12 @@ class Product < ActiveRecord::Base
 	mount_uploader :image, AvatarUploader
 
 	validates :description, :name, presence: true
-  validates :price_in_cents, numericality: {only_integer: true}
+  	validates :price_in_cents, numericality: {only_integer: true}
+  
+
+  	def formatted_price
+   		price_in_dollars = price_in_cents.to_f / 100
+    	sprintf("%.2f", price_in_dollars)
+  	end
+
 end
